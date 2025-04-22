c4ba984fc885274101768c3afd10f417378fc306
ShuFan-X <xsfkkk8255@163.com>
2025-04-22 21:59:33 +0800

    Update Predictor.py

--- Predictor.py
+++ Predictor.py
@@ -6,7 +6,7 @@ import shap
 import matplotlib.pyplot as plt
 from lime.lime_tabular import LimeTabularExplainer
 #pip install xgboost==2.0.3 --no-deps
-#import xgboost
+import xgboost
 #model = xgboost.Booster()
 #model.load_model('XGB.json')
 df2 =pd.read_csv('x_test.csv')
